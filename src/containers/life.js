f10968b5cc51b2ac191774a090ac57b2ce99ffaa
Tommy <tokutomi@degino.com>
2020-08-19 20:22:35 +0900

    feat: ブリンカーを組み込んだ。

--- src/containers/life.js
+++ src/containers/life.js
@@ -2,31 +2,41 @@ import React from 'react';
 import {atom, useRecoilState} from 'recoil'
 import Board from '../components/board'
 
+const blinker = [[
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 0, 0]
+], [
+  [0, 1, 0],
+  [0, 1, 0],
+  [0, 1, 0]
+]]
+
 const lifeState = atom({
-  key:      "lifeState",
-  default:  [
-    [1, 1, 1],
-    [1, 1, 1],
-    [0, 0, 0]
-  ]
+  key:          'lifeState',
+  default:  {
+    size:       3,
+    generation: 1,
+    status:     blinker[0]
+  }
 })
 
 const LifeContainer = ({...props}) => {
   const [life, setLife] = useRecoilState(lifeState)
   const handleForward = evt => {
-    setLife(state => [
-      [1, 0, 1],
-      [1, 0, 1],
-      [0, 0, 0]
-    ])
+    setLife(state => ({...state,
+      generation:   state.generation + 1,
+      status:       blinker[state.generation % 2]
+    }))
   }
 
   return (
     <Board
-      state         = {life}
+      state         = {life.status}
       handleForward = {handleForward}
     />
   )
 }
+
 
 export default LifeContainer
